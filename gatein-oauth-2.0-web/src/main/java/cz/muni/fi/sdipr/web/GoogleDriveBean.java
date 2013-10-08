ed20712d5b02ab73bd98dc59d8fcbd263723d360
matejkobza <matej.kobza@gmail.com>
2013-10-09 00:02:02 +0200

    new version of oauth 2 and google plus

--- gatein-oauth-2.0-web/src/main/java/cz/muni/fi/sdipr/web/GoogleDriveBean.java
+++ gatein-oauth-2.0-web/src/main/java/cz/muni/fi/sdipr/web/GoogleDriveBean.java
@@ -3,138 +3,130 @@ package cz.muni.fi.sdipr.web;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.http.HttpResponseException;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.jackson.JacksonFactory;
 import com.google.api.services.drive.Drive;
-import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.drive.model.File;
 import cz.muni.fi.sdipr.core.GoogleLoginBean;
-import java.io.IOException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
-import javax.annotation.PostConstruct;
-import java.io.Serializable;
-import java.util.Arrays;
-import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.inject.Inject;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.List;
 
 /**
  * @author matejkobza
  */
 @ManagedBean
 @SessionScoped
-public class GoogleDriveBean implements Serializable,OAuthAbstractBean {
+public class GoogleDriveBean implements Serializable, OAuthAbstractBean {
 
-  private static final long serialVersionUID = 7781881562444309512L;
-  private static Log log = LogFactory.getLog(GooglePlusBean.class);
-  private List<File> files = null;
-  private Drive drive = null;
-  
-  protected GoogleCredential credential;
-  @Inject
-  protected GoogleLoginBean loginBean;
+    private static final long serialVersionUID = 7781881562444309512L;
+    private static Log log = LogFactory.getLog(GooglePlusBean.class);
+    private List<File> files = null;
+    private Drive drive = null;
 
+    protected GoogleCredential credential;
 
-  @PostConstruct
-  public void GoogleDriveBean() {
-    System.out.println("@PostConstruct");
-    System.out.println("GoogleDriveBean");
-    loginBean.addScopes(Arrays.asList(DriveScopes.DRIVE));
-  }
+    @Inject
+    protected GoogleLoginBean loginBean;
 
-  public void loadDrive() {
-    if (credential == null) {
-      credential = loginBean.getCredential();
+    public GoogleDriveBean() {
+        System.out.println("@PostConstruct");
+        System.out.println("GoogleDriveBean");
+//        loginBean.addScopes(Arrays.asList(DriveScopes.DRIVE));
     }
 
-    if (credential == null) {
-      loginBean.requestAccessToken();
-    }
+//    public void loadDrive() {
+//        if (credential == null) {
+//            credential = loginBean.getCredential();
+//        }
+//
+//        if (credential == null) {
+//            loginBean.requestAccessToken();
+//        }
+//
+//        if (credential != null) {
+//            HttpTransport httpTransport = new NetHttpTransport();
+//            JacksonFactory jsonFactory = new JacksonFactory();
+//
+//            drive = new Drive.Builder(httpTransport, jsonFactory, credential)
+//                    .setApplicationName(APPLICATION_NAME)
+//                    .build();
+//        }
+//
+//    }
 
-    if (credential != null) {
-      HttpTransport httpTransport = new NetHttpTransport();
-      JacksonFactory jsonFactory = new JacksonFactory();
-
-      drive = new Drive.Builder(httpTransport, jsonFactory, credential)
-              .setApplicationName(APPLICATION_NAME)
-              .build();
-    }
-
-  }
-
-  public void loadFiles() {
-    if (drive == null) {
-      this.loadDrive();
-    }
-    try {
-      try {
-        files = drive.files().list().execute().getItems();
-      } catch (GoogleJsonResponseException ex) {
-        log.info("User is not authenticated to Google Drive", ex);
-      }
-    } catch (IOException ex) {
-      log.error(this + ".loadFiles", ex);
-    }
-  }
-
-  public List<File> getFiles() {
-    if (drive == null) {
-      this.loadDrive();
-    }
-
-    if (drive == null) {
-      return null;
-    } else {
-
-      if (files == null) {
-        loadFiles();
-      }
-
-      if (files != null) {
-        for (File file : files) {
-          System.out.println(file.getMimeType());
+    public void loadFiles() {
+        if (drive == null) {
+//            this.loadDrive();
         }
-      }
-
-      return files;
-    }
-  }
-
-  @Override
-  public Boolean getIsAuthenticated() {
-    if (this.drive == null) {
-      this.loadDrive();
-    }
-    if (drive != null) {
-      try {
         try {
-          drive.about().get().execute();
-          return true;
-        } catch (HttpResponseException ex) {
-          if (ex.getStatusCode() == 401) {
-            log.error("Credentials have been revoked", ex);
-            return false;
-          }
+            try {
+                files = drive.files().list().execute().getItems();
+            } catch (GoogleJsonResponseException ex) {
+                log.info("User is not authenticated to Google Drive", ex);
+            }
+        } catch (IOException ex) {
+            log.error(this + ".loadFiles", ex);
         }
-      } catch (IOException ex) {
-        log.error(this + ".getIsAuthenticated", ex);
-        return false;
-      }
     }
-    return false;
-  }
 
-  public void downloadFile(File file) {
-    System.out.println("downloadFile: " + file.getTitle());
-    System.out.println("webContentLink:\n" + file.getWebContentLink());
+    public List<File> getFiles() {
+        if (drive == null) {
+//            this.loadDrive();
+        }
 
-    System.out.println("Title: " + file.getTitle());
-    System.out.println("Description: " + file.getDescription());
-    System.out.println("MIME type: " + file.getMimeType());
-  }
+        if (drive == null) {
+            return null;
+        } else {
+
+            if (files == null) {
+                loadFiles();
+            }
+
+            if (files != null) {
+                for (File file : files) {
+                    System.out.println(file.getMimeType());
+                }
+            }
+
+            return files;
+        }
+    }
+
+    @Override
+    public Boolean getIsAuthenticated() {
+        if (this.drive == null) {
+//            this.loadDrive();
+        }
+        if (drive != null) {
+            try {
+                try {
+                    drive.about().get().execute();
+                    return true;
+                } catch (HttpResponseException ex) {
+                    if (ex.getStatusCode() == 401) {
+                        log.error("Credentials have been revoked", ex);
+                        return false;
+                    }
+                }
+            } catch (IOException ex) {
+                log.error(this + ".getIsAuthenticated", ex);
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public void downloadFile(File file) {
+        System.out.println("downloadFile: " + file.getTitle());
+        System.out.println("webContentLink:\n" + file.getWebContentLink());
+
+        System.out.println("Title: " + file.getTitle());
+        System.out.println("Description: " + file.getDescription());
+        System.out.println("MIME type: " + file.getMimeType());
+    }
 }
--- gatein-oauth-2.0-web/src/main/java/cz/muni/fi/sdipr/web/GooglePlusBean.java
+++ gatein-oauth-2.0-web/src/main/java/cz/muni/fi/sdipr/web/GooglePlusBean.java
@@ -1,25 +1,15 @@
 package cz.muni.fi.sdipr.web;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.client.googleapis.json.GoogleJsonResponseException;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.jackson.JacksonFactory;
-import com.google.api.services.plus.Plus;
-import com.google.api.services.plus.PlusScopes;
 import com.google.api.services.plus.model.Person;
 import cz.muni.fi.sdipr.core.GoogleLoginBean;
-import java.io.IOException;
-import java.io.Serializable;
-import java.util.Arrays;
-import javax.annotation.PostConstruct;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.inject.Inject;
-
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import java.io.Serializable;
 
 /**
  * @author matejkobza
@@ -28,58 +18,62 @@ import org.apache.commons.logging.LogFactory;
 @SessionScoped
 public class GooglePlusBean implements Serializable, OAuthAbstractBean {
 
-  private static final long serialVersionUID = 4317642879328909566L;
-  private static Log log = LogFactory.getLog(GooglePlusBean.class);
-  Person profile = null;
-  
-  protected GoogleCredential credential;
-  @Inject
-  protected GoogleLoginBean loginBean;
-  
-  @PostConstruct
-  public void GooglePlusBean() {
-    System.out.println("@PostConstruct");
-    System.out.println("GooglePlusBean");
-    loginBean.addScopes(Arrays.asList(PlusScopes.PLUS_LOGIN, PlusScopes.PLUS_ME));
-  }
+    private static final long serialVersionUID = 4317642879328909566L;
+    private static Log log = LogFactory.getLog(GooglePlusBean.class);
+    Person profile = null;
 
-  public void loadProfile() {
-    if (credential == null) {
-      credential = loginBean.getCredential();
-    }
+    private GoogleCredential credential;
 
-    if (credential == null) {
-      loginBean.requestAccessToken();
-    }
+    @Inject
+    private GoogleLoginBean loginBean;
 
-    if (credential != null) {
-      HttpTransport httpTransport = new NetHttpTransport();
-      JacksonFactory jsonFactory = new JacksonFactory();
-      
-      Plus plus = new Plus.Builder(httpTransport, jsonFactory, credential)
-              .setApplicationName(APPLICATION_NAME)
-              .build();
-      try {
-        try {
-          profile = plus.people().get("me").execute();
-        } catch (GoogleJsonResponseException ex) {
-          log.info("User is not authenticated to Google Plus", ex);
+    public GooglePlusBean() throws Exception {
+        System.out.println("@PostConstruct");
+        System.out.println("GooglePlusBean");
+        if (loginBean == null) {
+            throw new Exception("CDI is not working");
+        } else {
+//            loginBean.addScopes(Arrays.asList(PlusScopes.PLUS_LOGIN, PlusScopes.PLUS_ME));
         }
-      } catch (IOException ex) {
-        log.error("loadProfile", ex);
-      }
     }
-  }
 
-  public Person getProfile() {
-    return profile;
-  }
+    public void loadProfile() {
+        if (credential == null) {
+//            credential = loginBean.getCredential();
+        }
 
-  @Override
-  public Boolean getIsAuthenticated() {
-    if (profile == null) {
-      this.loadProfile();
+        if (credential == null) {
+//            loginBean.requestAccessToken();
+        }
+
+        if (credential != null) {
+//            HttpTransport httpTransport = new NetHttpTransport();
+//            JacksonFactory jsonFactory = new JacksonFactory();
+
+//            Plus plus = new Plus.Builder(httpTransport, jsonFactory, credential)
+//                    .setApplicationName(APPLICATION_NAME)
+//                    .build();
+//            try {
+//                try {
+//                    profile = plus.people().get("me").execute();
+//                } catch (GoogleJsonResponseException ex) {
+//                    log.info("User is not authenticated to Google Plus", ex);
+//                }
+//            } catch (IOException ex) {
+//                log.error("loadProfile", ex);
+//            }
+        }
     }
-    return profile != null;
-  }
+
+    public Person getProfile() {
+        return profile;
+    }
+
+    @Override
+    public Boolean getIsAuthenticated() {
+        if (profile == null) {
+            this.loadProfile();
+        }
+        return profile != null;
+    }
 }
